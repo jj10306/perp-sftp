b00f2e0eec4ecf29c77e1e50c4326b9776d0f6a2
Jakob Johnson <johnsonjakob99@gmail.com>
2020-07-06 13:37:45 -0500

    Add construct_tree

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 extern crate chrono;
 
-use std::{fs, env, io, boxed::Box, vec::Vec, ffi::OsString, ffi::OsStr, path::Path, convert::AsRef};
+use std::{fs, vec::Vec, string::String, path::PathBuf};
 use chrono::{Utc, DateTime};
 
 #[derive(Debug)]
 struct Node {
-    name: OsString,
+    name: String,
     last_modified: DateTime<Utc>,
     children: Vec<Node>,
     depth: u16
 }
 
 impl Node {
-    fn new(name: OsString) -> Node {
+    fn new(name: String, depth: u16) -> Node {
         Node {
             name,
             last_modified: Utc::now(),
             children: Vec::new(),
-            depth: 0
+            depth
         }
     }
-    fn add_child(&mut self, mut child: Node) {
-        child.set_depth(self.depth + 1);
+    fn add_child(&mut self, child: Node) {
         self.children.push(child)
     }
-    fn set_depth(&mut self, depth: u16) {
-        self.depth = depth;
-    }
+    #[allow(dead_code)]
     fn touch(&mut self) {
         self.last_modified = Utc::now();
     }
@@ -36,20 +33,22 @@ struct DirTree {
     root: Node
 }
 impl DirTree {
+    #[allow(dead_code)]
     fn new(root: Node) -> DirTree {
         DirTree {
             root
         }
     }
 }
-
-fn traverse_from<T: AsRef<Path>>(entry_point: T) {
+#[allow(dead_code)]
+fn traverse_from(entry_point: String) {
     if let Ok(dir_iter) = fs::read_dir(entry_point) {
         for entry in dir_iter {
             match entry {
                 Ok(entry) => {
                     println!("{:?}", entry.file_name());
-                    traverse_from(entry.path());
+                    let path_string = pathbuf_to_string(entry.path());
+                    traverse_from(path_string);
                 },
                 Err(_) => println!("sumn went wrong")
             }
@@ -57,43 +56,31 @@ fn traverse_from<T: AsRef<Path>>(entry_point: T) {
     } 
 }
 
-fn construct_tree<T: AsRef<Path>>(entry_point: T) -> Node {
-    let root = Node::new(
-        OsStr::into_os_string(Box::new(*(entry_point.as_ref().file_name().unwrap())))
+fn construct_tree(root: String, depth: u16) -> Node {
+    let mut node = Node::new(
+        root.clone(),
+        depth
     );
-    if let Ok(dir_iter) = fs::read_dir(entry_point) {
+    if let Ok(dir_iter) = fs::read_dir(root.clone()) {
         for entry in dir_iter {
             match entry {
                 Ok(entry) => {
-                    root.add_child(construct_tree(entry.path()));
+                    let path_string = pathbuf_to_string(entry.path());
+                    node.add_child(construct_tree(path_string, depth + 1));
                 },
                 Err(_) => println!("sumn went wrong")
             }
         } 
     }
-    root 
+    node
 
 }
-fn main() {
-    // let dir_iter = fs::read_dir("../root").unwrap();
-    // for entry in dir_iter {
-    //     match entry {
-    //         Ok(entry) => {
-    //             let metadata = entry.metadata();
-    //             println!("{:?}", entry);
-    //         },
-    //         Err(_) => println!("oops")
-    //     }
-    // }
-    traverse_from("../root");
-    // construct_tree()
+// converting PathBuf to String is verbose bc PathBuf isn't necessarily UTF-8 Encoded 
+fn pathbuf_to_string(pb: PathBuf) -> String {
+    pb.into_os_string().into_string().unwrap()
 }
-// fn construct_tree() {
-//     let mut root = Node::new();
-//     let c1 = Node::new();
-//     let c2 = Node::new();
-//     root.add_child(c1);
-//     root.add_child(c2);
-//     let tree = DirTree::new(root);
-//     println!("{:?}", tree);
-// }
+
+fn main() {
+    let root = construct_tree("../root".to_string(), 0);
+    println!("{:?}", root);
+}
